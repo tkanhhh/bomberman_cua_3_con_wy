fc2fce73b1121cfb07fe9db0396f1486af6e2ba6
Elyy27 <100121386+Elyy27@users.noreply.github.com>
2022-10-08 16:55:32 +0700

    Add files via upload

--- src/uet/oop/bomberman/entities/ActiveEntity/Weapon/Bomb.java
+++ src/uet/oop/bomberman/entities/ActiveEntity/Weapon/Bomb.java
@@ -7,52 +7,49 @@ import uet.oop.bomberman.entities.ActiveEntity.moveable.Bomber;
 import uet.oop.bomberman.entities.ActiveEntity.moveable.Enemy.Enemy;
 import uet.oop.bomberman.graphics.Sprite;
 import uet.oop.bomberman.utils.Sound;
-
 import java.util.ArrayList;
 import java.util.List;
 
-// Quang - Class định nghĩa lớp bomb
 public class Bomb extends ActiveEntity {
 
-    public boolean exploded;            // đã nổ chưa
-    protected int timeToExplode = 120;  // thời gian chờ trước khi nổ, 2s
-    protected int _timeAfter = 40;      // thời gian sau khi nổ
+    public boolean exploded;            // check if bomb exploded or not
+    protected int timeToExplode = 120;  // time before explode - 2s
+    protected int timeAfter = 40;      // time after explode
     public int bomberID;
-    public int radius;
+    public int length;
     public boolean added = false;
     private boolean played = false;
-    private boolean hasAnyOne = true;
 
     public List<Flame> flameList = new ArrayList<>();
 
 
     /**
-     * Constructor để tạo 1 quả bom.
-     * @param x toạ độ đơn vị
-     * @param y toạ độ đơn vị
-     * @param img hình ảnh
+     * Constructor to create a bomb
+     * @param x
+     * @param y
+     * @param img
      */
-    public Bomb(int x, int y, Image img, int bomberID, int radius) {
+    public Bomb(int x, int y, Image img, int bomberID, int length) {
         super(x, y, img);
-        this.exploded = false;                  // ban đầu bom chưa nổ
-        this.delete = false;                    // ban đầu bom chưa bị xoá
-        this.bomberID = bomberID;               // của bomber đặt quả bom này
-        this.radius = radius;                   // độ dài flame
+        this.exploded = false;                  // at first not explode
+        this.delete = false;                    // at first not delete
+        this.bomberID = bomberID;               // distinguish bomb from different players
+        this.length = length;                   // flame length
 
         BombermanGame.bombmap[this.getSmallY()][getSmallX()] = '@';
         BombermanGame.countbomb.put(bomberID, BombermanGame.countbomb.get(bomberID) + 1);
 
-        // for 4 hướng để tạo flamelist chứa các flame của vụ nổ
-        // flame chỉ dừng tạo tiếp khi gặp tường hoặc brick, flame có thể xuyên qua portal
-        for (int i = 1; i <= radius; i++) {
-            char whichType = BombermanGame.map[this.getSmallY()][this.getSmallX()+i];
+        // 4 directions of flame list contains flames
+        // flame stop if reach wall
+        for (int i = 1; i <= length; i++) {
+            char whichType = BombermanGame.map[this.getSmallY()][this.getSmallX() + i];
             if(whichType != '#' && whichType != '%') {
-                if ( i == radius || whichType == '*') { // nếu là cái cuối hoặc gặp tường
-                    this.flameList.add(new Flame(this.getSmallX()+i,this.getSmallY(),3,true,Sprite.explosion_vertical.getFxImage(),bomberID));
+                if (i == length || whichType == '*') { // if reach end or wall
+                    this.flameList.add(new Flame(this.getSmallX() + i, this.getSmallY(),3,true, Sprite.explosion_vertical.getFxImage(), bomberID));
                     break;
                 } else
                 {
-                    this.flameList.add(new Flame(this.getSmallX()+i,this.getSmallY(),3,false,Sprite.explosion_vertical.getFxImage(),bomberID));
+                    this.flameList.add(new Flame(this.getSmallX() + i, this.getSmallY(),3,false, Sprite.explosion_vertical.getFxImage(), bomberID));
                 }
             } else
             {
@@ -60,15 +57,15 @@ public class Bomb extends ActiveEntity {
             }
         }
 
-        for (int i = 1; i <= radius; i++) {
-            char whichType = BombermanGame.map[this.getSmallY()+i][this.getSmallX()];
+        for (int i = 1; i <= length; i++) {
+            char whichType = BombermanGame.map[this.getSmallY() + i][this.getSmallX()];
             if(whichType != '#' && whichType != '%') {
-                if ( i == radius || whichType == '*') {
-                    this.flameList.add(new Flame(this.getSmallX(),this.getSmallY()+i,1,true,Sprite.explosion_vertical.getFxImage(),bomberID));
+                if (i == length || whichType == '*') {
+                    this.flameList.add(new Flame(this.getSmallX(),this.getSmallY() + i,1,true,Sprite.explosion_vertical.getFxImage(), bomberID));
                     break;
                 } else
                 {
-                    this.flameList.add(new Flame(this.getSmallX(),this.getSmallY()+i,1,false,Sprite.explosion_vertical.getFxImage(),bomberID));
+                    this.flameList.add(new Flame(this.getSmallX(),this.getSmallY() + i,1,false,Sprite.explosion_vertical.getFxImage(), bomberID));
                 }
             } else
             {
@@ -76,15 +73,15 @@ public class Bomb extends ActiveEntity {
             }
         }
 
-        for (int i = 1; i <= radius; i++) {
-            char whichType = BombermanGame.map[this.getSmallY()][this.getSmallX()-i];
+        for (int i = 1; i <= length; i++) {
+            char whichType = BombermanGame.map[this.getSmallY()][this.getSmallX() - i];
             if(whichType != '#' && whichType != '%') {
-                if ( i == radius || whichType == '*') {
-                    this.flameList.add(new Flame(this.getSmallX()-i,this.getSmallY(),2,true,Sprite.explosion_vertical.getFxImage(),bomberID));
+                if (i == length || whichType == '*') {
+                    this.flameList.add(new Flame(this.getSmallX() - i,this.getSmallY(),2,true, Sprite.explosion_vertical.getFxImage(), bomberID));
                     break;
                 } else
                 {
-                    this.flameList.add(new Flame(this.getSmallX()-i,this.getSmallY(),2,false,Sprite.explosion_vertical.getFxImage(),bomberID));
+                    this.flameList.add(new Flame(this.getSmallX() - i, this.getSmallY(),2,false, Sprite.explosion_vertical.getFxImage(), bomberID));
                 }
             } else
             {
@@ -92,15 +89,15 @@ public class Bomb extends ActiveEntity {
             }
         }
 
-        for (int i = 1; i <= radius; i++) {
-            char whichType = BombermanGame.map[this.getSmallY()-i][this.getSmallX()];
+        for (int i = 1; i <= length; i++) {
+            char whichType = BombermanGame.map[this.getSmallY() - i][this.getSmallX()];
             if(whichType != '#' && whichType != '%') {
-                if ( i == radius || whichType == '*') {
-                    flameList.add(new Flame(this.getSmallX(),this.getSmallY()-i,0,true,Sprite.explosion_vertical.getFxImage(),bomberID));
+                if ( i == length || whichType == '*') {
+                    flameList.add(new Flame(this.getSmallX(),this.getSmallY() - i,0,true, Sprite.explosion_vertical.getFxImage(), bomberID));
                     break;
                 } else
                 {
-                    flameList.add(new Flame(this.getSmallX(),this.getSmallY()-i,0,false,Sprite.explosion_vertical.getFxImage(),bomberID));
+                    flameList.add(new Flame(this.getSmallX(),this.getSmallY() - i,0,false, Sprite.explosion_vertical.getFxImage(), bomberID));
                 }
             } else
             {
@@ -112,12 +109,11 @@ public class Bomb extends ActiveEntity {
 
     @Override
     public void update() {
-        if (timeToExplode > 0) { // Chưa nổ
-            timeToExplode--;    // Đếm ngược thời gian bom nổ
-            // Animation bom khi chưa nổ
-            setImg(Sprite.movingSprite(Sprite.bomb,Sprite.bomb_1,Sprite.bomb_2,timeToExplode, animationBetWeen).getFxImage());
-        } else { // Nổ rồi
-
+        if (timeToExplode > 0) { // not explode yet
+            timeToExplode--;    // countdown
+            // Animation when not explode yet
+            setImg(Sprite.movingSprite(Sprite.bomb, Sprite.bomb_1, Sprite.bomb_2, timeToExplode, animationBetWeen).getFxImage());
+        } else { // exploded
             if(!played) {
                 new Sound("sound/boom.wav","default") ;
                 played = true;
@@ -128,24 +124,23 @@ public class Bomb extends ActiveEntity {
                 added = true;
             }
             exploded = true;
-            _timeAfter--; // Đếm ngược thời gian bom sau khi nổ
-            if (_timeAfter < 0) { // Nếu đã hết thời gian sau khi nổ
-                delete = true; // Xoá
+            timeAfter--; // countdown
+            if (timeAfter < 0) {
+                delete = true; // Delete
                 BombermanGame.bombmap[getSmallY()][getSmallX()] = ' ';
-                BombermanGame.countbomb.put(bomberID,BombermanGame.countbomb.get(bomberID)-1);
-                //System.out.println(BombermanGame.countbomb.get(bomberID));
+                BombermanGame.countbomb.put(bomberID, BombermanGame.countbomb.get(bomberID) - 1);
             }
-            // Animation bom nổ
-            setImg(Sprite.movingSprite(Sprite.bomb_exploded,Sprite.bomb_exploded1,Sprite.bomb_exploded2,_timeAfter, animationBetWeen).getFxImage());
+            // Animation when bomb explode
+            setImg(Sprite.movingSprite(Sprite.bomb_exploded, Sprite.bomb_exploded1, Sprite.bomb_exploded2, timeAfter, animationBetWeen).getFxImage());
         }
     }
 
     @Override
     public void collide(ActiveEntity entity) {
-        if(!exploded) { // bomb chưa nổ thì ko check va chạm
+        if(!exploded) { // unnecessary to check collision if bomb hasn't exploded yet
             return;
         }
-        if(entity.isDead) { // entity đó chết rồi thì thôi
+        if(entity.isDead) { // unnecessary to check collision if entity is dead
             return;
         }
         if (!entity.isDead && this.getSmallX() == entity.getSmallX() && this.getSmallY() == entity.getSmallY()) {
--- src/uet/oop/bomberman/entities/ActiveEntity/Weapon/Flame.java
+++ src/uet/oop/bomberman/entities/ActiveEntity/Weapon/Flame.java
@@ -9,81 +9,80 @@ import uet.oop.bomberman.entities.ActiveEntity.moveable.Bomber;
 import uet.oop.bomberman.entities.ActiveEntity.moveable.Enemy.Enemy;
 import uet.oop.bomberman.graphics.Sprite;
 
-//Quang - Class Flame để tạo ra đối tượng Flame khi bom nổ
+//Create flame when bomb explode
 public class Flame extends ActiveEntity {
 
-    private final int bomberID; // bomber voi id nay dat qua bom nay
-    public int direction;       // 0-up , 1-down , 2-left , 3-right  Hướng của flame, truyền vào lúc tạo đối tượng
-    protected int timeToExplode = 0; // thời gian đếm ngược trước khi xuất hiện các flame, 2s
-    public int _timeAfter = 40;        // thời gian xuất hiện các flame - đếm ngược
-    public boolean last;               // nếu là Flame cuối cùng ( ở đuôi ) thì true
+    private final int bomberID;      // distinguish bomb from different players
+    public int direction;            // 0-up , 1-down , 2-left , 3-right - direction of flame
+    protected int timeToExplode = 0; // countdown time before show up flame
+    public int timeAfter = 40;      // countdown time flame showing up
+    public boolean last;             // check last flame length
 
     /**
-     * Tạo 1 đối tượng flame với toạ độ đơn vị x,y, hướng của flame, và có là flame cuối cùng hay không
-     * @param x toạ độ đơn vị x - trục ngang
-     * @param y toạ độ đơn vị y - trục dọc
-     * @param direction hướng ,0-up , 1-down , 2-left , 3-right
-     * @param last nếu là flame cuối cùng thì true
-     * @param img hình ảnh- không quan trọng, hàm update sẽ tự chọn dựa theo direction và last
+     * @param x
+     * @param y
+     * @param direction 0-up , 1-down , 2-left , 3-right
+     * @param last last flame length
+     * @param img
      */
     public Flame(int x, int y, int direction, boolean last, Image img, int bomberID) {
         super(x, y, img);
         this.direction = direction;
         this.last = last;
-        this.show = false;  // ban đầu chưa hiển thị, sau khi đếm ngược xong mới hiển thị
-        this.delete = false;   // chưa xoá, hiển thị xong xuôi mới xoá
+        this.show = false;
+        this.delete = false;
         this.bomberID = bomberID;
     }
 
     @Override
     public void update() {
 
-        if (timeToExplode > 0) { // Chưa nổ
-            timeToExplode--;    // Đếm ngược tới lúc nổ
-        } else { // Nổ rồi
-            show = true;    // hiển thị
-            _timeAfter--;   // đếm ngược thời gian hiển thị
+        if (timeToExplode > 0) { // not explode
+            timeToExplode--;    // countdown to explode
+        } else { // exploded
+            show = true;    // show
+            timeAfter--;   // countdown
 
-            if (_timeAfter < 0) {   // nếu hết thời gian hiển thị thì xoá
-                delete = true;     // xoá
+            if (timeAfter < 0) {
+                delete = true;
             }
 
             if (direction == 0 || direction == 1 && !last) { // vertical - not last
-                setImg(Sprite.movingSprite(Sprite.explosion_vertical, Sprite.explosion_vertical1, Sprite.explosion_vertical2, _timeAfter, animationBetWeen).getFxImage());
+                setImg(Sprite.movingSprite(Sprite.explosion_vertical, Sprite.explosion_vertical1, Sprite.explosion_vertical2, timeAfter, animationBetWeen).getFxImage());
             }
             if (direction == 0 && last) { // vertical - up - last
-                setImg(Sprite.movingSprite(Sprite.explosion_vertical_top_last, Sprite.explosion_vertical_top_last1, Sprite.explosion_vertical_top_last2, _timeAfter, animationBetWeen).getFxImage());
+                setImg(Sprite.movingSprite(Sprite.explosion_vertical_top_last, Sprite.explosion_vertical_top_last1, Sprite.explosion_vertical_top_last2, timeAfter, animationBetWeen).getFxImage());
             }
             if (direction == 1 && last) { // vertical - down - last
-                setImg(Sprite.movingSprite(Sprite.explosion_vertical_down_last, Sprite.explosion_vertical_down_last1, Sprite.explosion_vertical_down_last2, _timeAfter, animationBetWeen).getFxImage());
+                setImg(Sprite.movingSprite(Sprite.explosion_vertical_down_last, Sprite.explosion_vertical_down_last1, Sprite.explosion_vertical_down_last2, timeAfter, animationBetWeen).getFxImage());
             }
 
             if (direction == 2 || direction == 3 && !last) { // horizontal - not last
-                setImg(Sprite.movingSprite(Sprite.explosion_horizontal, Sprite.explosion_horizontal1, Sprite.explosion_horizontal2, _timeAfter, animationBetWeen).getFxImage());
+                setImg(Sprite.movingSprite(Sprite.explosion_horizontal, Sprite.explosion_horizontal1, Sprite.explosion_horizontal2, timeAfter, animationBetWeen).getFxImage());
             }
             if (direction == 2 && last) { // horizontal - left - last
-                setImg(Sprite.movingSprite(Sprite.explosion_horizontal_left_last, Sprite.explosion_horizontal_left_last1, Sprite.explosion_horizontal_left_last2, _timeAfter, animationBetWeen).getFxImage());
+                setImg(Sprite.movingSprite(Sprite.explosion_horizontal_left_last, Sprite.explosion_horizontal_left_last1, Sprite.explosion_horizontal_left_last2, timeAfter, animationBetWeen).getFxImage());
             }
             if (direction == 3 && last) { // horizontal - right - last
-                setImg(Sprite.movingSprite(Sprite.explosion_horizontal_right_last, Sprite.explosion_horizontal_right_last1, Sprite.explosion_horizontal_right_last2, _timeAfter, animationBetWeen).getFxImage());
+                setImg(Sprite.movingSprite(Sprite.explosion_horizontal_right_last, Sprite.explosion_horizontal_right_last1, Sprite.explosion_horizontal_right_last2, timeAfter, animationBetWeen).getFxImage());
             }
         }
     }
 
     /**
-     * Check Va Chạm giữa Flame và các Entity khác
-     * Flame chạm Active Entity nào cũng kill nó luôn, trừ powerup(không xử lí thuộc tính is dead)
-     * @param entity đối tượng cần check va chạm với
+     * Check collision between flame and other entities
+     * Any active entity collide with flame must dead, except for power up
+     * @param entity
      */
     @Override
     public void collide(ActiveEntity entity) {
-        if(!show) { // flame chưa hiện thì chưa check va chạm
+        if(!show) { // unnecessary to check
             return;
         }
-        if(entity.isDead) {
+        if(entity.isDead) { // unnecessary to check
             return;
         }
-        if(entity instanceof Flame) { // không check va chạm với flame khác
+        if(entity instanceof Flame) { // // unnecessary to check with other flame
             return;
         }
         if (entity instanceof Bomb && this.getSmallX() == entity.getSmallX() && this.getSmallY() == entity.getSmallY()) {
